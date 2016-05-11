c631ebecdaff44facd856814d6016d66e9769489
Darwin <darwin@centralarticle.com>
2016-05-11 15:48:24 -0500

    Added orders and users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,18 @@ Depot::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :test
+  
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "darwinrc@gmail.com",
+  #  authentication: "plain",
+  #  user_name: "darwinrc",
+  #  password: "secret",
+  #  enable_starttls_auto: true
+  #}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
